0a4379a89c677ca0d9ae22d5b24b7d81b3c27cbe
Carlos J Palacios <cjpm1983@gmail.com>
2021-01-15 11:19:33 -0500

    Subiendo formulario en portada con fechas2

---
+++ hostal/static/assets/dtpicker/How to Use Date Picker with Django_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 comments","multiple":"{num} comments","one":"1 comment"}},"counts":[{"id":"\/tutorial\/2019\/01\/03\/how-to-use-date-picker-with-django","comments":32}]});
+}
