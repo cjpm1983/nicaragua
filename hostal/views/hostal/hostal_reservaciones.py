5dc9a7b21562025c49b2e1f5a5d562c077be8ac7
Carlos J Palacios <cjpm1983@gmail.com>
2021-01-31 02:50:11 -0500

    Antes de paginar

--- hostal/views/hostal/hostal_reservaciones.py
+++ hostal/views/hostal/hostal_reservaciones.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from hostal.models import Reservacion
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @login_required(login_url="")
 def ReservacionesView(request):
